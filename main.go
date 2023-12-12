1e918f71f46b3bf3482b9832ce207aded88585d6
jenish6ek <151835263+jenish6ek@users.noreply.github.com>
2023-12-12 07:02:00 +0600

    1st commit

--- main.go
+++ main.go
@@ -1,21 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Privet"))
-}
+import "fmt"
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	log.Println("Zapusk web-server")
-	err := http.ListenAndServe(":8080", mux)
-	log.Fatal(err)
-
 	fmt.Println("Hello, World!")
 }
