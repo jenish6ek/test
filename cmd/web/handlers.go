0113b7f39061d78c995b98663e0d40fe95d84d11
jenish6ek <151835263+jenish6ek@users.noreply.github.com>
2024-02-13 07:40:29 +0600

    thrd commit

---
+++ cmd/web/handlers.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"strconv"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		app.NotFound(w)
+		return
+	}
+
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		app.serverError(w, err)
+	}
+}
+
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.NotFound(w)
+		return
+	}
+	fmt.Fprintf(w, "Otobrajenie opredelennoi zametki s ID %d...", id)
+}
+
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		app.clientError(w, http.StatusMethodNotAllowed)
+		return
+	}
+
+	title := "История про улитку"
+	content := "Улитка выползла из раковины, /nвытянула рожки, /nи опять подобрала их."
+	expires := "7"
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+
+	w.Write([]byte("sozdanie novoi zametki..."))
+}
